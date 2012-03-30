8f9e06e763060dd1b09f568e0425a25755f066a4
Sebastian <sebastian.urchs@gmail.com>
2012-03-30 18:53:13 -0400

    added a wrapper for everything

--- wrapper.py
+++ wrapper.py
@@ -6,3 +6,45 @@ Created on Fri Mar 30 14:58:22 2012
 
 wrapper script
 """
+
+import os
+import sys
+from . import preprocesser_v7 as pp
+from . import machinetamer as mt
+
+sysPath = os.path.abspath(os.curdir)
+batchFile = sys.argv[1]
+configFile = sys.argv[2]
+
+print 'Welcome to wrapper '
+print 'All variables have been read in '
+print 'Let\'s go '
+
+print '\nStarting the Preprocessing '
+(feature, ages) = pp.Main(batchFile,
+                          configFile,
+                          sysPath,
+                          saveOut=0)
+
+print '\nStarting SVM '
+(trueKeep, predKeep, trainModel) = mt.Processer(feature,
+                                                ages)
+print 'Done with SVM\n '
+
+print 'Saving TrueKeep '
+pp.TexSaver(trueKeep,
+            (sysPath + '/output'),
+            prefix='trueVals',
+            suffix='temp')
+
+print 'Saving PredKeep '
+pp.TexSaver(predKeep,
+            (sysPath + '/output'),
+            prefix='predVals',
+            suffix='temp')
+
+print 'Saving TrainModel '
+pp.TexSaver(trainModel,
+            (sysPath + '/output'),
+            prefix='trainModel',
+            suffix='temp')
